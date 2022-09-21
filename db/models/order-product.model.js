71ea1148f4319698d4db6a7d8e8dfbf50475bcd8
mdiazc <miguelangel.diaz.kommit@gmail.com>
2022-09-21 13:00:11 -0500

    Fixed: git conflicts

---
+++ db/models/order-product.model.js
@@ -0,0 +1,66 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+const { ORDER_TABLE } = require('./order.model');
+const { PRODUCT_TABLE } = require('./product.model');
+
+const ORDER_PRODUCT_TABLE = 'orders-products';
+
+const OrderSchema = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+
+  createdAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+    field: 'created_at',
+    defaultValue: Sequelize.NOW,
+  },
+
+  orderId: {
+    field: 'order_id',
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    unique: false,
+    references: {
+      model: ORDER_PRODUCT_TABLE,
+      key: 'id',
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL',
+  },
+  productId: {
+    field: 'product_id',
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    unique: false,
+    references: {
+      model: PRODUCT_TABLE,
+      key: 'id',
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL',
+  },
+};
+
+class Order extends Model {
+  static associate(models) {
+    models.Product;
+  }
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: ORDER_TABLE,
+      modelName: 'Order',
+      timestamps: false,
+    };
+  }
+}
+
+module.exports = {
+  ORDER_TABLE,
+  OrderSchema,
+  Order,
+};
